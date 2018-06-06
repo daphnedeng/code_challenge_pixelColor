3f363da223d7b8732ad5394390507e9f5cb78d3c
Daphne <dolllikewawa@gmail.com>
2018-06-05 23:23:15 -0700

    use MVC for scalability

--- design.js
+++ design.js
@@ -1,37 +1,157 @@
-const canvas = document.querySelector('#canvas');
-let timer = 2000;
-
-const color = () => {
-    return '#FF00AA';
-}
-
-const makeGrid = () => {
-    let tr, td;
-    for (let i = 0; i < 4; i++) {
-        tr = document.createElement('tr');
-        canvas.appendChild(tr);
-        for (let j = 0; j < 4; j++) {
-            td = document.createElement('td');
-            tr.appendChild(td);
-            td.setAttribute('class', 'table-cells')
+/* ======= Model ======= */
+const data = {
+    newColor: '#FF00AA',
+    currentCell: null,
+    cells: [
+        {
+            color: '#d6f5ff',
+            time: 2000
+        // },
+        // {
+        //     color: '#d6f5ff',
+        //     time: 2000
+        // },
+        // {
+        //     color: '#d6f5ff',
+        //     time: 2000
+        // },
+        // {
+        //     color: '#d6f5ff',
+        //     time: 2000
+        // },
+        // {
+        //     color: '#d6f5ff',
+        //     time: 2000
+        // },
+        // {
+        //     color: '#d6f5ff',
+        //     time: 2000
+        // },
+        // {
+        //     color: '#d6f5ff',
+        //     time: 2000
+        // },
+        // {
+        //     color: '#d6f5ff',
+        //     time: 2000
+        // },
+        // {
+        //     color: '#d6f5ff',
+        //     time: 2000
+        // },
+        // {
+        //     color: '#d6f5ff',
+        //     time: 2000
+        // },
+        // {
+        //     color: '#d6f5ff',
+        //     time: 2000
+        // },
+        // {
+        //     color: '#d6f5ff',
+        //     time: 2000
+        // },
+        // {
+        //     color: '#d6f5ff',
+        //     time: 2000
+        // },
+        // {
+        //     color: '#d6f5ff',
+        //     time: 2000
+        // },
+        // {
+        //     color: '#d6f5ff',
+        //     time: 2000
+        // },
+        // {
+        //     color: '#d6f5ff',
+        //     time: 2000
+        // }
         }
-    }   
-}
+    ]
+};
 
-setInterval(() => {
-    let cells = document.querySelectorAll('.table-cells');
-       timer -= 1000;
-       /* if (current color === previous color) {
-           return;
-        } elseif (timer <= 0) {
-            currentColor = color();
-            one of the cell.style.backgroundColor = currentColor;
-            previousColor = currentColor;
-        } else {
-            return;
-        }*/
-}, 1000);
+/* ======= Controller ======= */
+const controller = {
+
+    init: function() {
+        data.currentCell = data.cells[0];
+        updateColor.init();
+        grid.init();
+    },
+
+    getCurrentCell: function() {
+        return data.currentCell;
+    },
+
+    getNewColor: function() {
+        return data.newColor;
+    },
+
+    setCurrentCell: function(cell) {
+        data.currentCell = cell;
+    },
+
+    updateCell: function() {
+        grid.render();
+    }
+};
 
 
-makeGrid();
-color();
+/* ======= View ======= */
+const grid = {
+
+    init: function() {
+        this.canvas = document.querySelector('#canvas');
+        this.render();
+    },
+
+    render: function() {
+        let tr, td;
+        let currentCell = controller.getCurrentCell();
+        for (let i = 0; i < 4; i++) {
+            tr = document.createElement('tr');
+            canvas.appendChild(tr);
+            for (let j = 0; j < 4; j++) {
+                td = document.createElement('td');
+                tr.appendChild(td);
+                td.style.backgroundColor = currentCell.color;
+                td.setAttribute('class', 'table-cells');
+            }
+        }  
+    }    
+};
+
+const updateColor = {
+
+    init: function() {
+        this.cells = document.querySelectorAll('table-cells');
+        this.render();
+    },
+
+    render: function() {
+
+        let newColor = controller.getNewColor();
+        let currentCell = controller.getCurrentCell(); 
+        let previousColor = currentCell.color;
+        let timer = currentCell.time;
+
+        setInterval(() => {
+            timer -= 250;
+            if (previousColor === newColor) {
+                return;
+            } else if (timer <= 0) {                
+                previousColor = newColor;
+                return function(newCell) {
+                    controller.setCurrentCell(newCell);
+                    grid.render();
+                }
+            } else {
+                return;
+            }
+        }, 250)
+    }  
+};
+
+/* ======= Go! ======= */
+controller.init();
