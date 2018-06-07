e5eee766a2437f027577cfa8107d72784a9f218b
Daphne <dolllikewawa@gmail.com>
2018-06-07 15:27:47 -0700

    randomly select a cell to apply colors

--- design.js
+++ design.js
@@ -2,9 +2,27 @@
 
 const canvas = document.querySelector('#canvas');
 
-let time = 2000;
+let time = 2000, init_state = [], i = 0;
+
+let getTimeStamp = () => {
+    return new Date().getTime()
+};
+
+const genColor = () => {
+    let color='rgb('+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+')';
+    return color;
+};
 
 const makeGrid = () => {
+    while (i < 4*4) {
+        //store time, as the "previous timestamp"
+        init_state.push({
+            timestamp: getTimeStamp()
+        });
+        i++;
+    }
+
+    //make a 4x4 table
     let tr, td;
     for (let i = 0; i < 4; i++) {
         tr = document.createElement('tr');
@@ -17,11 +35,18 @@ const makeGrid = () => {
 }
 
 setInterval(() => {
-    let tdLen = canvas.querySelectorAll('td').length;
-    let num_columns = canvas.rows[0].cells.length;
-    let tdIndex = Math.floor(Math.random() * tdLen);
-    let randomtd = canvas.rows[Math.floor(tdIndex/num_columns)].cells[tdIndex % num_columns];
-    randomtd.classList.toggle('table-cells');
+    let td = canvas.querySelectorAll('td');
+    // fandom index 0-15
+    let tdIndex = Math.floor(Math.random() * td.length);
+    //get current time
+    let now = getTimeStamp(); 
+    let state = init_state[tdIndex];
+    //compare time of one of cells
+    if (now - state.timestamp >= time) {        
+        td[tdIndex].style.backgroundColor = genColor();
+        //update timestamp
+        state.timestamp = now;
+    }  
 }, 250)
 
 makeGrid();
