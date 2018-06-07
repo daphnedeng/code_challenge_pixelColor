6d1e33ab968bfc44a13d8efef2e4bce750805ae4
Daphne <dolllikewawa@gmail.com>
2018-06-06 20:40:37 -0700

    randomly select a cell and apply color

--- design.js
+++ design.js
@@ -1,157 +1,28 @@
-/* ======= Model ======= */
-const data = {
-    newColor: '#FF00AA',
-    currentCell: null,
-    cells: [
-        {
-            color: '#d6f5ff',
-            time: 2000
-        // },
-        // {
-        //     color: '#d6f5ff',
-        //     time: 2000
-        // },
-        // {
-        //     color: '#d6f5ff',
-        //     time: 2000
-        // },
-        // {
-        //     color: '#d6f5ff',
-        //     time: 2000
-        // },
-        // {
-        //     color: '#d6f5ff',
-        //     time: 2000
-        // },
-        // {
-        //     color: '#d6f5ff',
-        //     time: 2000
-        // },
-        // {
-        //     color: '#d6f5ff',
-        //     time: 2000
-        // },
-        // {
-        //     color: '#d6f5ff',
-        //     time: 2000
-        // },
-        // {
-        //     color: '#d6f5ff',
-        //     time: 2000
-        // },
-        // {
-        //     color: '#d6f5ff',
-        //     time: 2000
-        // },
-        // {
-        //     color: '#d6f5ff',
-        //     time: 2000
-        // },
-        // {
-        //     color: '#d6f5ff',
-        //     time: 2000
-        // },
-        // {
-        //     color: '#d6f5ff',
-        //     time: 2000
-        // },
-        // {
-        //     color: '#d6f5ff',
-        //     time: 2000
-        // },
-        // {
-        //     color: '#d6f5ff',
-        //     time: 2000
-        // },
-        // {
-        //     color: '#d6f5ff',
-        //     time: 2000
-        // }
+'use strict';
+
+const canvas = document.querySelector('#canvas');
+
+let time = 2000;
+
+const makeGrid = () => {
+    let tr, td;
+    for (let i = 0; i < 4; i++) {
+        tr = document.createElement('tr');
+        canvas.appendChild(tr);
+        for (let j = 0; j < 4; j++) {
+            td = document.createElement('td');
+            tr.appendChild(td);
         }
-    ]
-};
+    } 
+}
 
-/* ======= Controller ======= */
-const controller = {
+setInterval(() => {
+    let tdLen = canvas.querySelectorAll('td').length;
+    let num_columns = canvas.rows[0].cells.length;
+    let tdIndex = Math.floor(Math.random() * tdLen);
+    let randomtd = canvas.rows[Math.floor(tdIndex/num_columns)].cells[tdIndex % num_columns];
+    randomtd.classList.toggle('table-cells');
+}, 250)
 
-    init: function() {
-        data.currentCell = data.cells[0];
-        updateColor.init();
-        grid.init();
-    },
+makeGrid();
 
-    getCurrentCell: function() {
-        return data.currentCell;
-    },
-
-    getNewColor: function() {
-        return data.newColor;
-    },
-
-    setCurrentCell: function(cell) {
-        data.currentCell = cell;
-    },
-
-    updateCell: function() {
-        grid.render();
-    }
-};
-
-
-/* ======= View ======= */
-const grid = {
-
-    init: function() {
-        this.canvas = document.querySelector('#canvas');
-        this.render();
-    },
-
-    render: function() {
-        let tr, td;
-        let currentCell = controller.getCurrentCell();
-        for (let i = 0; i < 4; i++) {
-            tr = document.createElement('tr');
-            canvas.appendChild(tr);
-            for (let j = 0; j < 4; j++) {
-                td = document.createElement('td');
-                tr.appendChild(td);
-                td.style.backgroundColor = currentCell.color;
-                td.setAttribute('class', 'table-cells');
-            }
-        }  
-    }    
-};
-
-const updateColor = {
-
-    init: function() {
-        this.cells = document.querySelectorAll('table-cells');
-        this.render();
-    },
-
-    render: function() {
-
-        let newColor = controller.getNewColor();
-        let currentCell = controller.getCurrentCell(); 
-        let previousColor = currentCell.color;
-        let timer = currentCell.time;
-
-        setInterval(() => {
-            timer -= 250;
-            if (previousColor === newColor) {
-                return;
-            } else if (timer <= 0) {                
-                previousColor = newColor;
-                return function(newCell) {
-                    controller.setCurrentCell(newCell);
-                    grid.render();
-                }
-            } else {
-                return;
-            }
-        }, 250)
-    }  
-};
-
-/* ======= Go! ======= */
-controller.init();
